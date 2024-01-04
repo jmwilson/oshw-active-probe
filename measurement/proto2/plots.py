57094309bdaebae2662a2c942227b265a09e1a05
James Wilson <jmw@jmw.name>
2024-01-04 11:29:37 -0800

    Add port extension in probe impedance calculation

--- measurement/proto2/plots.py
+++ measurement/proto2/plots.py
@@ -6,6 +6,7 @@ import math
 from matplotlib.ticker import EngFormatter
 
 Z0 = 50
+c0 = 299792458
 
 # Quick and dirty Touchstone file reader for S1P
 def read_s1p_file(fd):
@@ -37,14 +38,20 @@ def X_C(freq, C):
 
 data = pd.read_csv("probe_insertionloss.csv", skiprows=51, header=None)
 with open("probed_line_returnloss.s1p") as fd:
-    freq, s11 = read_s1p_file(fd)
+    freq, s11_probed_line = read_s1p_file(fd)
 
 with open("line_returnloss.s1p") as fd:
     _,s11_line = read_s1p_file(fd)
 
-z_data = Z0 * (1 + s11) / (1 - s11)
+# add a port extension to account for measured electrical length from calibration plane to board edge of exposed line
+meas_plane_shift = 14.839e-3
+port_extension = np.exp(1j*2*meas_plane_shift*2*np.pi/c0*freq)
+s11_probed_line = port_extension * s11_probed_line
+s11_line = port_extension * s11_line
+
+z_probed_line = Z0 * (1 + s11_probed_line) / (1 - s11_probed_line)
 z_line = Z0 * (1 + s11_line) / (1 - s11_line)
-z_probe = 1/(1/z_data - 1/z_line)
+z_probe_only = 1/(1/z_probed_line - 1/z_line)
 min_freq_index = (freq >= 1e8).argmax()
 
 plt.figure(figsize=(10,6))
@@ -63,7 +70,7 @@ plt.savefig("probe_s21.svg", format='svg')
 
 plt.figure(figsize=(10,6))
 ax = plt.axes()
-plt.plot(freq[min_freq_index:], abs(z_probe[min_freq_index:]))
+plt.plot(freq[min_freq_index:], abs(z_probe_only[min_freq_index:]))
 trend_freq = np.logspace(8, 9, 20)
 plt.plot(trend_freq, X_C(trend_freq, 1e-12))
 ax.annotate("1 pF", xy=(800e6, X_C(800e6, 1e-12)), xycoords='data', xytext=(50,30), textcoords='offset points', arrowprops=dict(arrowstyle='->', facecolor='black'))
